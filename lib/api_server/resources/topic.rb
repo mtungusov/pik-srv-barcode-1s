638966c161fcf876108ea6d3e210824dd71ebbc0
TM <tm@macadmin.ru>
2015-12-22 17:14:07 +0300

    Resources::Topic skel

--- lib/api_server/resources/topic.rb
+++ lib/api_server/resources/topic.rb
@@ -3,25 +3,43 @@ class Resources::Topic < Resources::JsonAuthResource
     %w[POST]
   end
 
-  # def to_json
-  #   {
-  #       result: {
-  #           data: _data(_topic_name)
-  #       }
-  #   }.to_json
-  # end
+  def process_post
+    data, err = _check_params
+    unless err
+      r, err = _process(data)
+    end
+    result = if err
+               ApiServer.logger.debug "Error: #{err}" if $DEBUG
+               { error: err }
+             else
+               { result: r }
+             end
+    response.body = result.to_json
+    true
+  end
+
+  def _process(data)
+    ApiServer.logger.debug "Data: #{data}" if $DEBUG
+    [{offset: 99}, nil]
+  end
 
   private
+
+  def _check_params
+    body, err = _params
+    ApiServer.logger.debug "Body: #{body}" if $DEBUG
+    return [nil, err] if err
+    return [nil, {message: 'Empty body'}] if body.nil?
+    return [nil, {message: 'Body is not Hash'}] unless body.is_a? Hash
+    return [nil, {message: 'Empty body'}] if body.empty?
+    data = body[:data]
+    return [nil, {message: 'Empty data'}] if data.nil?
+    return [nil, {message: 'Data is not Array'}] unless data.is_a? Array
+    return [nil, {message: 'Empty data'}] if data.empty?
+    [data, nil]
+  end
 
   def _topic_name
     request.path_info[:topic_name]
   end
-
-  # def _data(topic_name)
-  #   [{}, {}]
-  # end
-
-  # def _offset
-  #   request.query['offset'] || 0
-  # end
 end
